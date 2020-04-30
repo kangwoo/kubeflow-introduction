46685d9e81bbb94188e1b344e1852d9ced0a9aac
kangwoo <kangwoo@gmail.com>
2020-04-30 20:45:11 +0900

    Add PyTorchJob

---
+++ 04-model-training/pytorchjob/build_mpi_image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+image_name=kangwoo/training-mpi-pytorchjob
+image_tag=0.0.1
+full_image_name=${image_name}:${image_tag}
+
+cd "$(dirname "$0")"
+
+docker build -t "$full_image_name" . -f Dockerfile-mpi
+docker push "$full_image_name"
+
